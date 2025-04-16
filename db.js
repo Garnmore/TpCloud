51d23c3773a49c4e5b1e4ab764131fe7b7e16045
Garnmore <161452308+Garnmore@users.noreply.github.com>
2025-04-16 10:25:33 +0200

    Update db.js

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const { Sequelize } = require('sequelize')
 
 // database
 const sequelize = new Sequelize(
-  ProcessingInstruction.env.DB_NAME , // Database name
+  ProcessingInstruction.env.DB_NAME, // Database name
   ProcessingInstruction.env.DB_USER, // User
   ProcessingInstruction.env.DB_PASSWORD, // Password
   {
